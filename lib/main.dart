0fe7c28fe7552ecf1037732138aac81ab632bf43
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-09-24 00:48:35 -0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//The main is the starting point for all flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home:Scaffold(appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+  );
+}
