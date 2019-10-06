411512c588e068d0de5239a776a8d7da8b827a56
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-10-06 17:08:22 -0400

    refactor to external widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
+
 //The main is the starting point for all flutter apps
 void main() {
   runApp(
-    MaterialApp(
+    new MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I am Rich'),
+          title: Text('I Am So Not   Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
-          child: Image(
+          child: Image( 
             image: AssetImage('images/diamond.png'),
             //image: NetworkImage('https://cnet3.cbsistatic.com/img/KiI_dLHwMGFlGsnGuYbsDJdbON4=/970x0/2019/01/11/21c3dece-7bbb-4fba-8fca-10c894b8b39a/starship.jpg'),
 //            alignment: Alignment.center,
@@ -18,7 +30,7 @@ void main() {
         ),
         backgroundColor: Colors.blueGrey,
       ),
-    ),
-  );
+    );
+  }
 }
 
